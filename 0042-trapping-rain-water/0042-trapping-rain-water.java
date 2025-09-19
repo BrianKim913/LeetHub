462abd8c7ef6a49bcce84c2852d2aede6886593a
Bumsoo Kim <119075610+BrianKim913@users.noreply.github.com>
2025-09-19 13:56:35 +0900

    0042-trapping-rain-water Hard 0 ms (100%) 47 MB (8.78%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        int res = 0;
-        int left = 0;
+        
+        int left =0;
         int right = height.length-1;
         int leftMax = height[left];
         int rightMax = height[right];
 
-        while (left<right){
-            leftMax = Math.max(height[left],leftMax);
-            rightMax = Math.max(height[right],rightMax);
+        int res = 0;
 
-            if(leftMax<=rightMax){
+        while(left<right){
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if(rightMax>=leftMax){
                 res += leftMax-height[left];
                 left++;
+                continue;
             }
-
             else{
-                res += rightMax-height[right];
+                res+= rightMax-height[right];
                 right--;
+                continue;
             }
-        
-        
-        }
+        } 
 
         return res;
-        
     }
 }
